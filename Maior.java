244b3568fbc0b3ad7ab8a59c0773750ced20283f
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2021-09-06 21:40:06 -0300

    update

--- Maior.java
+++ Maior.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class Maior {
 	public static void main (String args[]) {
 		Scanner entrada = new Scanner(System.in);
-	    int i, x=0, valor;
+	    int i, x=0, valor; //Declaração de variáveis do tipo inteiro(int)
 	    for (i=1;i<9;i++) {
 	       System.out.printf("\nDigite o %d valor: ",i);
 	       valor = entrada.nextInt();
 	       if(valor > x) x = valor;
-	     }
-	     System.out.printf("%d\n",x);
+	     } //Teste do maior valor
+	     System.out.printf("\nO maior valor digitado é: %d",x); //Impressão do maior valor
 	}
 }
